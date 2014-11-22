6e1f534c9047cfdf2d3e285982c2e6a32691b135
Jake Humphrey <jakebhumphrey@gmail.com>
2014-11-22 18:21:21 +0100

    added comments to MultFS

--- MultFS.h
+++ MultFS.h
@@ -9,6 +9,10 @@
 
 using namespace std;
 
+//! Top-Level Class for Multimedia Objects
+/*!
+* This class contains all the objects and groups in the program, and is responsible for destroying or creating them.
+*/
 class MultFS 
 {
 private:
@@ -20,10 +24,19 @@ private:
 
 public:
 
+  //! Adds a new object to the object container
   intrusive_ptr<BaseObject> create(BaseObject*);
+
+  //! Adds a new group to the groups container
   intrusive_ptr<Group> create(Group*);
+
+  //! Removes objects and groups by name
   void remove(const string&);
+
+  //! Prints out details of objects and groups with the given name
   void search(const string&);
+
+  //! Plays objects and groups with the given name
   void play(const string&);
 };
 #endif
