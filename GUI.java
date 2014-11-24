f49b02e98e2895ac124ec1ecd289bc1a936b0720
Jake Humphrey <jhumphrey@c128-08.enst.fr>
2014-11-24 10:19:14 +0100

    added menu

--- GUI.java
+++ GUI.java
@@ -1,8 +1,4 @@
-import javax.swing.JFrame;
-import javax.swing.JTextArea;
-import javax.swing.JButton;
-import javax.swing.JPanel;
-import javax.swing.JScrollPane;
+import javax.swing.*;
 import java.awt.BorderLayout;
 import java.awt.event.*; 
 
@@ -16,6 +12,12 @@ public class GUI extends JFrame
     JButton button1 = new JButton("Button 1");
     JButton button2 = new JButton("Button 2");
     JButton button3 = new JButton("Button 3");
+    JMenuBar menubar = new JMenuBar();
+    JMenu menu = new JMenu("menu");
+    JMenuItem menuitem1 = new JMenuItem("Action 1");
+    JMenuItem menuitem2 = new JMenuItem("Action 2");
+    JMenuItem menuitem3 = new JMenuItem("Action 3");
+    JToolBar toolbar = new JToolBar("toolbar");
 
     public static void main(String args[])
     {
@@ -37,26 +39,43 @@ public class GUI extends JFrame
 	button2.addActionListener(new AddTextAction2());
 	button3.addActionListener(new CloseListener());
 
+	menuitem1.addActionListener(new AddTextAction1());
+	menuitem2.addActionListener(new AddTextAction2());
+	menuitem3.addActionListener(new CloseListener());
+
+	menu.add(menuitem1);
+	menu.add(menuitem2);
+	menu.add(menuitem3);
+
+	toolbar.add(new AddTextAction1());
+	toolbar.add(new AddTextAction2());
+	toolbar.add(new CloseListener());
+
+	menubar.add(menu);
+	menubar.add(toolbar);
+
+	add(menubar, BorderLayout.NORTH);
+
 	pack();
 	setVisible(true);
     }
 
     //! Action to be performed when button 1 is pressed
-    private class AddTextAction1 implements ActionListener { 
+    private class AddTextAction1 extends AbstractAction { 
 	public void actionPerformed(ActionEvent e) { 
 	    text.append("You pressed button1\n"); 
 	} 
     } 
 
     //! Action to be performed when button 2 is pressed
-    private class AddTextAction2 implements ActionListener { 
+    private class AddTextAction2 extends AbstractAction { 
 	public void actionPerformed(ActionEvent e) { 
 	    text.append("You pressed button2\n"); 
 	} 
     } 
 
     //! Action to be performed when button 3 is pressed
-    private class CloseListener implements ActionListener { 
+    private class CloseListener extends AbstractAction { 
 	public void actionPerformed(ActionEvent e) { 
 	    System.exit(0);
 	} 
